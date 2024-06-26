067a854c3130b9addd723e90a99c41660a9bb7fd
Müller Esposito Nunes <mulleresposito@hotmail.com>
2024-06-26 02:33:20 -0300

    refactor(Header): it adds the handler of the onChange and onKeyDown events to add a new task;

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,31 @@ import { PlusCircle } from "@phosphor-icons/react";
 
 import logo from "../assets/logo.svg";
 import styles from "./Header.module.css";
+import { useState } from "react";
 
-export function Header() {
+interface HeaderProps {
+  onNewTaskCreated: (taskValue: string) => void;
+}
+
+export function Header({ onNewTaskCreated }: HeaderProps) {
+  const [taskValue, setNewTaskValue] = useState("");
+
+  const handleOnNewTaskCreated = () => {
+    onNewTaskCreated(taskValue);
+    setNewTaskValue("");
+  }
+
   return (
    <header className={styles.header}>
     <img src={logo} className={styles.logo} />
     <div className={styles.newTaskBox}>
-      <input placeholder="Adicione uma nova tarefa" />
-      <button>
+      <input 
+        placeholder="Adicione uma nova tarefa" 
+        value={taskValue}
+        onChange={e => setNewTaskValue(e.target.value)}
+        onKeyDown={e => e.key === "Enter" ? handleOnNewTaskCreated() : "" }
+      />
+      <button onClick={handleOnNewTaskCreated}>
         Criar 
         <PlusCircle size={18} color="#f2f2f2" weight="bold" />
       </button>
