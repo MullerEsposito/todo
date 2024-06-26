00e9352d932e390cb239bdf598d339e9c71c467c
Müller Esposito Nunes <mulleresposito@hotmail.com>
2024-06-25 23:12:35 -0300

    refactor(ListTasks): it updates the ListTasks component to use the Task component;

--- src/components/ListTasks/index.tsx
+++ src/components/ListTasks/index.tsx
@@ -1,18 +1,31 @@
 import { ClipboardText } from "@phosphor-icons/react";
 import styles from "./index.module.css";
+import { useState } from "react";
+import { Task } from "./Task";
 
 export function ListTasks() {
+  // const [tasks, setTasks] = useState<Task[]>([]);
+  const tasks: Task[] = [];
   const root = document.documentElement;
 
   const blueColor = getComputedStyle(root).getPropertyValue('--blue').trim();
   const purpleColor = getComputedStyle(root).getPropertyValue('--purple').trim();
+
+  tasks.push({id: 1, content: "Tarefa 1"});
+  tasks.push({id: 2, content: "Tarefa 2"});
+  tasks.push({id: 3, content: "Tarefa 3"});
+
+  type Task = {
+    id: number;
+    content: string;
+  }
 
   return (
     <div className={styles.container}>
       <header className={styles.header}>
         <div className={styles.info}>
           <p style={{ color: blueColor }}>Tarefas criadas</p>
-          <span>0</span>
+          <span>{tasks ? tasks.length : 0}</span>
         </div>
         <div className={styles.info}>
           <p style={{ color: purpleColor }}>Concluídas</p>
@@ -21,11 +34,19 @@ export function ListTasks() {
       </header>
 
       <main className={styles.main}>
-        <ClipboardText size={56} />
-        <p>
-          Você ainda não tem tarefas cadastradas
-          <span>Crie tarefas e organize seus itens a fazer</span>
-        </p>
+        {tasks.length ? (
+          tasks.map((task: Task) => (
+            <Task key={task.id} id={task.id} content={task.content} />
+          ))
+        ):(
+          <>
+            <ClipboardText size={56} />
+            <p>
+              Você ainda não tem tarefas cadastradas
+              <span>Crie tarefas e organize seus itens a fazer</span>
+            </p>
+          </>
+        )}
       </main>
     </div>
   )
