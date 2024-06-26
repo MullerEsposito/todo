59ea68fea26aa5ec6f53d74a8bf641fb4408df4e
Müller Esposito Nunes <mulleresposito@hotmail.com>
2024-06-26 02:34:49 -0300

    refactor(App): it adds some handlers to handle with the Create, Update and Delete events;

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,48 @@
+import { useState } from "react";
+
 import { Header } from "./components/Header";
 import { ListTasks } from "./components/ListTasks";
 
+
 import "./global.css";
+import { v4 as uuidv4 } from "uuid";
+import { Task } from "./components/ListTasks/Task";
 
 function App() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const handleOnNewTaskCreated = (newTaskContent: string) => {
+    const newTask: Task = {
+      id: uuidv4(),
+      content: newTaskContent,
+      isCompleted: false
+    }
+    setTasks(oldTask => [...oldTask, newTask]);
+  }
+
+  const handleOnChangeTask = (taskId: string) => {
+    const updatedTasks = tasks.map(task => {
+      return task.id === taskId 
+        ? { ...task, isCompleted: !task.isCompleted }
+        : { ...task };
+    });
+
+    setTasks(updatedTasks);
+  }
+
+  const handleOnDeleteTask = (taskId: string) => {
+    const filteredTasks = tasks.filter(task => task.id != taskId);
+    setTasks(filteredTasks);
+  }
 
   return (
     <>
-      <Header />
-      <ListTasks />
+      <Header onNewTaskCreated={handleOnNewTaskCreated} />
+      <ListTasks 
+        tasks={tasks} 
+        onDeleteTask={handleOnDeleteTask}
+        onChangeTask={handleOnChangeTask}
+      />
     </>
   )
 }
