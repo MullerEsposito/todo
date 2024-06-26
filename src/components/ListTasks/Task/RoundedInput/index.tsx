485f60ebb1541951a18681fd9145790d60943d02
Müller Esposito Nunes <mulleresposito@hotmail.com>
2024-06-26 00:12:32 -0300

    refactor(RoundedInput): it creates the props interface of the component to be controlled by its parent component;

--- src/components/ListTasks/Task/RoundedInput/index.tsx
+++ src/components/ListTasks/Task/RoundedInput/index.tsx
@@ -2,13 +2,19 @@ import { InputHTMLAttributes } from "react"
 import styles from "./RoundedInput.module.css"
 
 interface RoundedInputProps extends InputHTMLAttributes<HTMLInputElement> {
-
+  isChecked: boolean;
+  handleIsChecked: () => void;
 }
 
-export function RoundedInput({ id }: RoundedInputProps) {
+export function RoundedInput({ id, isChecked, handleIsChecked }: RoundedInputProps) {
   return (
     <div className={styles.container}>
-      <input type="checkbox" name="" id={`task${id}`} />
+      <input 
+        type="checkbox" 
+        id={`task${id}`} 
+        checked={isChecked} 
+        onClick={handleIsChecked}
+      />
       <span className={styles.inputCheck} />
     </div>
   )
